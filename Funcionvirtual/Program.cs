6ef93a98cb494c28d9e163677ca0a762a0897bba
Mitzy521 <159392464+Mitzy521@users.noreply.github.com>
2025-12-18 21:07:16 -0600

    proyecto

---
+++ Funcionvirtual/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+class Empleado
+{
+    public string? Nombre;
+    public virtual double CalcularPago()
+    {
+        return 0;
+    }
+}
+
+class Gerente
+{
+    public double SalarioBase;
+    public double Bono;
+    public override double CalcularPago()
+    {
+        return SalarioBase + Bono;
+    }
+}
+
+class Operario : Empleado
+{
+    public int Horas;
+    public double TarifaHora;
+    public override double CalcularPago()
+    {
+        return Horas * TarifaHora;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Empleado> empleados = new List<Empleado>
+        {
+            new Gerente {Nombre = "Ana", SalarioBase = 3000, Bono = 500},
+            new Operario {Nombre = "Luis", Horas = 40, TarifaHora = 25}
+        };
+
+        foreach (Empleado e in empleados)
+        {
+            Console.WriteLine($"{e.Nombre}, gana Q{e.CalcularPago()}");
+        }
+    }
+}
+*/
+
+class Animal
+{
+    public virtual void HacerSonido()
+    {
+        Console.WriteLine("Los Animales Hacen un Sonido: ");
+    }
+}
+class Perro : Animal
+{
+    public override void HacerSonido()
+    {
+        Console.WriteLine("Guau");
+    }
+}
+class Gato : Animal
+{
+    public override void HacerSonido()
+    {
+        Console.WriteLine("Miau");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Animal> animales = new List<Animal>
+        {
+            new Animal(),
+            new Perro(),
+            new Gato()
+        };
+        foreach (Animal a in animales)
+        {
+            a.HacerSonido();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+/*
+namespace EjemploInterfaz
+{
+    interface IVehiculo
+    {
+        void Conducir();
+        void Detener();
+    }
+    class Automovil : IVehiculo
+    {
+        public void Conducir()
+        {
+            Console.WriteLine("El automóvil se está conduciendo");
+        }
+        public void Detener()
+        {
+            Console.WriteLine("El automóvil se ha detenido");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            IVehiculo miVehiculo = new Automovil();
+            miVehiculo.Conducir();
+            miVehiculo.Detener();
+        }
+    }
+}
+*/
+/*
+namespace EjMensaje
+{
+    interface IMensaje
+    {
+        void Mensaje();
+    }
+    class Email : IMensaje
+    {
+        public void Mensaje()
+        {
+            Console.WriteLine("Hola, Email");
+        }
+    }
+    class Sms : IMensaje
+    {
+        public void Mensaje()
+        {
+            Console.WriteLine("Hola, Mensaje de Texto");
+        }
+    }
+    class What : IMensaje
+    {
+        public void Mensaje()
+        {
+            Console.WriteLine("Hola, WhatsApp");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            List<IMensaje> mensajes = new List<IMensaje>
+            {
+                new Email(),
+                new Sms(),
+                new What()
+            };
+            foreach (IMensaje m in mensajes)
+            {
+                m.Mensaje();
+            }
+        }
+    }
+}
+*/
+/*
+namespace EjemploIterfaz
+{
+    interface IMensaje
+    {
+        void Enviarmensaje(string mensaje);
+    }
+
+    class Email : IMensaje
+    {
+        public void Enviarmensaje(string mensaje)
+        {
+            Console.WriteLine("Enviando email con el mensaje: " + mensaje);
+        }
+    }
+    class SMS : IMensaje
+    {
+        public void Enviarmensaje(string mensaje)
+        {
+            Console.WriteLine("Enviando SMS con el mensaje: " + mensaje);
+        }
+    }
+    class WhatsApp : IMensaje
+    {
+        public void Enviarmensaje(string mensaje)
+        {
+            Console.WriteLine("Enviando WhatsApp con el mensaje: " + mensaje);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            IMensaje email = new Email();
+            email.Enviarmensaje("Hola desde el email");
+            IMensaje sms = new SMS();
+            sms.Enviarmensaje("Hols desde el SMS");
+            IMensaje whatsapp = new WhatsApp();
+            whatsapp.Enviarmensaje("Hola desde el WhatsApp");
+        }
+    }
+}
+*/
+
+interface IMensaje
+{
+    void Enviarmensaje(string mensaje);
+}
+interface Operacion
+{
+    void Encender();
+    void Apagar();
+}
+
+class SmartTV : Operacion
+{
+    public void Encender()
+    {
+        Console.WriteLine("La Smart TV está encendida");
+    }
+    public void Apagar()
+    {
+        Console.WriteLine("La Smart TV está apagada");
+    }
+    public void EnviarMensaje(string mensaje)
+    {
+        Console.WriteLine("enviando mensaje desde la Smart TV: " + mensaje);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SmartTV miSmartTV = new SmartTV();
+        miSmartTV.Encender();
+        miSmartTV.EnviarMensaje("Hola desde la Smart TV");
+        miSmartTV.Apagar();
+    }
+}
