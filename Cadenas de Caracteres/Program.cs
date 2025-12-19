af4ed6a97ed4883f730ca65e54bba9d23221a010
Mitzy521 <159392464+Mitzy521@users.noreply.github.com>
2025-12-18 21:09:45 -0600

    proyecto

---
+++ Cadenas de Caracteres/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string str = "The brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMenssage = str.ToCharArray();
+//reverse the chars
+Array.Reverse(charMenssage);
+int x = 0;
+// count the o's
+foreach (char i in charMenssage)
+{
+    if (i == 'o')
+    {
+        x++;
+    }
+}
+//convert it back to a string
+string new_message = new string(charMenssage);
+//print in out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
